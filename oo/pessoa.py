7298973cac673d751e387ae82e44e559412fa358
felipe.nogueira <felipematheus.ns0@gmail.com>
2021-03-02 19:38:35 -0300

    Criado método de classe e método estático, onde no primeiro eu tenho acesso aos atributos da classe e no segundo é um método indiferente da classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,22 @@
-
 class Pessoa:
-    def __init__(self, *filhos, nome = None, idade=35):
+    olhos = 2
+
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+
     def cumprimentar(self):
         return 'Olá'
+
+    @staticmethod
+    def estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     matheus = Pessoa(nome='Matheus')
@@ -23,3 +34,8 @@ if __name__ == '__main__':
     del felipe.filhos
     print(felipe.__dict__)
     print(matheus.__dict__)
+    print(felipe.olhos)
+    print(id(felipe.olhos), id(matheus.olhos))
+
+    print(felipe.estatico())
+    print(felipe.nome_e_atributos_de_classe())
