2274202e00d01eb6a35d3afc1423754bf14e756b
felipe.nogueira <felipematheus.ns0@gmail.com>
2021-03-02 19:23:41 -0300

    Criado atributo complexo *filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,18 @@
 
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
-    p = Pessoa('Felipe')
-    print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    matheus = Pessoa(nome='Matheus')
+    felipe = Pessoa(matheus, nome='Felipe')
+    print(Pessoa.cumprimentar(felipe))
+    print(felipe.cumprimentar())
+    print(felipe.nome)
+    print(felipe.idade)
+    for filho in felipe.filhos:
+        print(filho.nome)
